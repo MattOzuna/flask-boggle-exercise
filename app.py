667567b3c87cf0ab0ecb7319d496f143b36e4fc5
Matt Ozuna <m.ozuna22@gmail.com>
2023-11-15 06:43:19 -0500

    completed through section3

--- app.py
+++ app.py
@@ -16,3 +16,35 @@ boggle_game = Boggle()
 def home_page():
     '''Start page w/ button for initializing the game board'''
     return render_template('home.html')
+
+@app.route('/start-game', methods=['POST'])
+def game_start():
+    '''
+    saves game board to session and redirects to gameboard page
+    '''
+    board = boggle_game.make_board()
+    session['board'] = board
+    session['responses'] = []
+
+    return redirect('/game-board')
+
+@app.route('/game-board')
+def game_board():
+    '''rendered game board'''
+    board = session['board']
+
+    return render_template('board.html', board= board)
+
+@app.route('/guess', methods=['POST'])
+def handle_guess():
+    '''
+    handle the guess
+    '''
+
+    answer = request.form['guess']
+    
+    responses = session['responses']
+    responses.append(answer)
+    session['responses'] = responses
+    
+    return redirect('/game-board')
---
+++ static/app.js
@@ -0,0 +1,5 @@
+
+let appendGif = async function(e){
+    e.preventDefault()
+    let url = await axios.get
+}
